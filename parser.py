8921a3abf3993e74b287ae1b578a4b0fcdc63684
ahernot <70817470+ahernot@users.noreply.github.com>
2022-05-08 11:44:01 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,9 +1,13 @@
 
 
+# goal of the program: apply specified modifications, and add some random ones too if desired
+"""
+will not treat:
+vertical alignment
+horizontal alignment
+"""
 
-
-
-
+# for colors: color names / hex tags / RGB(A) values
 
 
 
@@ -13,16 +17,33 @@ class Block:
     def __init__ (self, text: str, **kwargs):
         self.text = text
 
+        self.font_color = kwargs.get('font_color', 'black')  # to change
+        self.font_color_start = kwargs.get('font_color_start', self.font_color)
+        self.font_color_end = kwargs.get('font_color_end', self.font_color_start)
 
-        """
-        font-color-start
-        font-color-end #=start
-        background-color-start
-        background-color-end #=start
-        font-size-func #or val
+        self.background_color = kwargs.get('background_color', 'white')  # to change
+        self.background_color_start = kwargs.get('background_color_start', self.background_color)
+        self.background_color_end = kwargs.get('background_color_end', self.background_color_start)
 
-        """
+        self.font_size = kwargs.get('font_size', 12)  # to change
+        # process whether function / array / value
 
+        self.style_underlined = kwargs.get('underlined', False)
+        self.style_italicised = kwargs.get('italicised', False)
+        self.style_bold = kwargs.get('bold', False)
+        self.style_strikethrough = kwargs.get('strikethrough', False)
+
+        self.font = kwargs.get('font', None) # to change
+
+        # Generate
+        self.html = self.__generate()
+
+
+    def __generate (self):
+
+        html = ''
+        return html
+    
 
     
 
@@ -34,8 +55,18 @@ class Paragraph:
 
     def __init__ (self, text: str):
         self.text = text
+
+        self.parse = self.__parse()
+        self.html = self.__generate()
     
     def __parse (self):
+        # linear read? or multi split?
+        #\b{}  or \bold\
+
+        # multi split along all markers like \b, \bold, … and then determine arguments and ranges
+        pass
+
+    def __generate (self):
         pass
 
 
